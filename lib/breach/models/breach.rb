32d673f62bab575cd8d5e446686944cbfab07138
Stefan Penner <stefan.penner@gmail.com>
2011-02-19 20:04:41 -0600

    stats

--- lib/breach/models/breach.rb
+++ lib/breach/models/breach.rb
@@ -3,14 +3,15 @@ module Breach
     one_to_many :hash, :key => :hash_breach_id, :class => 'Breach::Hash'
 
     def hash_count
-      hash_dataset.count
+      hash_dataset.sum(:hash_count)
     end
 
     def cracked_count
-      hash_dataset.filter('hash_password_id > 0').count
+      hash_dataset.filter('hash_password_id > 0').sum(:hash_count)
     end
 
     def cracked_percentage
+      return 0 if hash_count == 0
       cracked_count/hash_count.to_f
     end
   end
