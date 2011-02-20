942c5fc23d45055c5a673e311abebfc2e737051a
Stefan Penner <stefan.penner@gmail.com>
2011-02-20 15:58:41 -0600

    rename, and add another association.

--- lib/breach/models/breach.rb
+++ lib/breach/models/breach.rb
@@ -1,18 +1,22 @@
 module Breach
   class Breach < Sequel::Model(:breach)
-    one_to_many :hash, :key => :hash_breach_id, :class => 'Breach::Hash'
+    one_to_many :hashes, :key => :hash_breach_id, :class => 'Breach::Hash'
 
+    one_to_many( :cracked_hashes,
+                 :key   => :hash_breach_id,
+                 :class => 'Breach::Hash') do |hashes|
+      hashes.filter { hash_password_id > 0}
+    end
 
     cache_methods do
       def hash_count
-        hash_dataset.sum(:hash_count).to_i
+        hashes_dataset.sum(:hash_count).to_i
       end
 
       def cracked_count
-        hash_dataset.filter('hash_password_id > 0').sum(:hash_count).to_i
+        cracked_hashes.sum(:hash_count).to_i
       end
     end
-
 
     def cracked_percentage
       return 0 if hash_count == 0
