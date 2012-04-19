c435804e89a1faa66c55c0637ef39ccccaefd49a
Ron <ron@skullsecurity.net>
2012-04-18 20:33:59 -0500

    Finished the code to accept submissions over the Web - everything appears to be working as expected now.

--- scripts/web.rb
+++ scripts/web.rb
@@ -8,6 +8,7 @@ require 'passwordcache'
 require 'hashtypes'
 require 'masks'
 require 'submissions'
+require 'submissionbatches'
 
 require 'pagination'
 
@@ -601,11 +602,27 @@ end
 
 post '/submissions/submit' do
   # IMPORTANT: these parameters need to be sanitized
-  submissions = params['passwords']
-  cracker     = Mysql::quote(params['cracker'])
+  submissions  = params['passwords']
+  cracker_name = Mysql::quote(params['cracker'])
+
 
   # TODO: Create a batch
   # TODO: Get the cracker
+
+  # Insert or find the cracker by their name
+  cracker = Crackers.query_ex({ :where => "`cracker_name`='#{cracker_name}'"})
+  if(cracker.size > 0)
+    cracker_id = cracker[0]['cracker_id']
+  else
+    cracker_id = Crackers.insert_rows({'cracker_name' => cracker_name})
+  end
+
+  # Insert a new batch, and get the id
+  batch_id = SubmissionBatches.insert_rows({
+      'submission_batch_cracker_id' => cracker_id,
+      'submission_batch_date' => Time.new().strftime('%Y-%m-%d'),
+      'submission_batch_ip' => @env['REMOTE_ADDR']
+  })
 
   # Automatically detect if the passwords are in hash:password format by
   # looking at the first bunch and seeing if they all contain colons
@@ -621,6 +638,7 @@ post '/submissions/submit' do
   words = []
   hashes = []
   submissions.each do |submission|
+    # IMPORTANT: This value has to be sanitized
     submission = Mysql::quote(submission)
     hash = ''
     if(with_hash)
@@ -638,7 +656,9 @@ post '/submissions/submit' do
     hashes << hash
   end
 
+  # This is the value that is inserted into the database
   values = {}
+  values['submission_submission_batch_id'] = batch_id
   values['submission_password'] = words
   values['submission_hash'] = hashes
 
